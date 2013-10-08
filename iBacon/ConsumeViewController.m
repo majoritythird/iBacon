39090f2264c323c1004d00b1dc7b39f1b24d6b87
Wes Gibbs <wesgibbs@gmail.com>
2013-10-08 10:02:24 -0700

    Initial consumer work

--- iBacon/ConsumeViewController.m
+++ iBacon/ConsumeViewController.m
@@ -6,9 +6,15 @@
 //  Copyright (c) 2013 Wes Gibbs. All rights reserved.
 //
 
+#import <CoreLocation/CoreLocation.h>
 #import "ConsumeViewController.h"
 
 @interface ConsumeViewController ()
+<CLLocationManagerDelegate>
+
+@property(nonatomic,strong) CLBeaconRegion *beaconRegion;
+@property(nonatomic,strong) CLLocationManager *locManager;
+@property(nonatomic,weak) IBOutlet UITextView *consumeLogTextView;
 
 @end
 
@@ -16,18 +22,94 @@
 
 #pragma mark - Lifecycle
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithCoder:(NSCoder *)aDecoder
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+  self = [super initWithCoder:aDecoder];
+
+  if (self) {
+    _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:[self proximityUUID] identifier:kMTBeaconIdentifier];
+    _locManager = [[CLLocationManager alloc] init];
+    self.locManager.delegate = self;
+  }
+
+  return self;
 }
 
 #pragma mark - Methods
 
+- (void)logIt:(NSString *)message
+{
+  [self logIt:message boolValue:-1];
+}
+
+- (void)logIt:(NSString *)message boolValue:(BOOL)boolValue
+{
+  if (boolValue > -1) {
+    NSString *formatString = [message stringByAppendingString:@": %d"];
+    NSLog(formatString, boolValue);
+    formatString = [@"\n" stringByAppendingString:formatString];
+    self.consumeLogTextView.text = [self.consumeLogTextView.text stringByAppendingString:[NSString stringWithFormat:formatString, boolValue]];
+
+  }
+  else {
+    NSLog(@"%@", message);
+    NSString *newLinedMessage = [@"\n" stringByAppendingString:message];
+    self.consumeLogTextView.text = [self.consumeLogTextView.text stringByAppendingString:newLinedMessage];
+  }
+}
+
+- (NSUUID *)proximityUUID
+{
+  NSString *uuidString = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString *)kMTProximityUUIDKey];
+  return [[NSUUID alloc] initWithUUIDString:uuidString];
+}
+
 - (IBAction)stopConsuming:(id)sender {
+  [self.locManager stopMonitoringForRegion:self.beaconRegion];
+  self.doneBlock();
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
+{
+  [self logIt:@"didDetermineState" boolValue:state];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+  [self logIt:@"@didFailWithError"];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+  [self logIt:@"didEnterRegion"];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+  [self logIt:@"didExitRegion"];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
+{
+  [self logIt:@"didStartMonitoringForRegion"];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+  [self logIt:@"didUpdateLocations"];
+}
+
+- (void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error
+{
+  [self logIt:@"monitoringDidFailForRegion"];
+}
+
+#pragma mark - UIAlertVewDelegate
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
   self.doneBlock();
 }
 
@@ -36,7 +118,28 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+  BOOL isMonitoringAvailableForClass = [CLLocationManager isMonitoringAvailableForClass:[self.beaconRegion class]];
+  CLAuthorizationStatus authorizationStatus = [CLLocationManager authorizationStatus];
+
+  [self logIt:@"locationServicesEnabled" boolValue:[CLLocationManager locationServicesEnabled]];
+  [self logIt:@"isRangingAvailable" boolValue:[CLLocationManager isRangingAvailable]];
+  [self logIt:@"authorizationStatus" boolValue:authorizationStatus];
+  [self logIt:@"isMonitoringAvailableForClass" boolValue:isMonitoringAvailableForClass];
+
+  if (!isMonitoringAvailableForClass) {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Hardware not supported" message:@"Your device does not support this feature." delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    [alertView show];
+  }
+  else if (authorizationStatus < kCLAuthorizationStatusAuthorized) {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"App not authorized" message:@"This app is currently not authorized to use location services. Fix it." delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    [alertView show];
+  }
+  else if (NO) { // HOW TO CHECK THAT BLUETOOTH IS ACTUALLY ON?
+  }
+  else {
+    [self.locManager startMonitoringForRegion:self.beaconRegion];
+  }
 }
 
 @end
